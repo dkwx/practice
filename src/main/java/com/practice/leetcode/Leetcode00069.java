fba7b03e6e12962f70cddf50bd94d16c01d063cb
kai.dai <kai.dai@qunar.com>
2020-05-09 13:24:57 +0800

    每日一题

---
+++ src/main/java/com/practice/leetcode/Leetcode00069.java
@@ -0,0 +1,59 @@
+package com.practice.leetcode;
+
+/**
+ * 实现 int sqrt(int x) 函数。
+ *
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ *
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * 示例 1:
+ *
+ * 输入: 4
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ *      由于返回类型是整数，小数部分将被舍去。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sqrtx
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author : kai.dai
+ * @date : 2020-05-03 21:18
+ */
+public class Leetcode00069 {
+
+
+    class Solution {
+        public int mySqrt(int x) {
+            if (x == 0 || x == 1) {
+                return x;
+            }
+            long start = 0, end = x;
+            long mid = (start + end) / 2;
+            while (start <= end) {
+                long v = mid * mid;
+                if (v == x) {
+                    break;
+                } else if (v > x) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+                mid = (start + end) / 2;
+            }
+            return (int)mid;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Leetcode00069().new Solution();
+        System.out.println(solution.mySqrt(4));
+    }
+}
