841b7f8f06d8d7d9a54b137edd4d87031e3f7e0f
kai.dai <kai.dai@qunar.com>
2020-05-15 09:11:22 +0800

    提交题目

---
+++ src/main/java/com/practice/leetcode/assist/Node.java
@@ -0,0 +1,17 @@
+package com.practice.leetcode.assist;
+
+/**
+ * @author : kai.dai
+ * @date : 2020-05-15 09:10
+ */
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
