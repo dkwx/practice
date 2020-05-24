2bf5069592780514ad0329302a933ebd039b3d24
kai.dai <kai.dai@qunar.com>
2020-05-24 17:41:14 +0800

    版本号概念

---
+++ src/main/java/com/practice/concurrent/ConcurrentHashMapTest.java
@@ -0,0 +1,14 @@
+package com.practice.concurrent;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author : kai.dai
+ * @date : 2020-05-24 16:39
+ */
+public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap<String, String> hashMap = new ConcurrentHashMap<>();
+
+    }
+}
