4530d251dc2eb4fbf1a7f7f0c8647eb945a8fd9f
kai.dai <kai.dai@qunar.com>
2020-05-27 23:38:03 +0800

    提交试题模板

--- src/main/java/com/practice/niuke/offers/Offers999.java
+++ src/main/java/com/practice/niuke/offers/Offers999.java
@@ -7,6 +7,12 @@ package com.practice.niuke.offers;
  * @date : 2020-05-27 13:34
  */
 public class Offers999 {
+    class Solution {
+        public int fib(int n) {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
