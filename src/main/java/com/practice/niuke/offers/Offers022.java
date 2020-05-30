234fc5e815f82099f1a72e006bb4282fbb83a3af
kai.dai <kai.dai@qunar.com>
2020-05-30 14:14:10 +0800

    剑指offer

---
+++ src/main/java/com/practice/niuke/offers/Offers022.java
@@ -0,0 +1,45 @@
+package com.practice.niuke.offers;
+
+import com.practice.leetcode.assist.ListNode;
+
+/**
+ * 剑指offer
+ *
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。例如，一个链表有6个节点，从头节点开始，它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个节点是值为4的节点。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ *
+ * 返回链表 4->5.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * @author : kai.dai
+ * @date : 2020-05-27 13:34
+ */
+public class Offers022 {
+    class Solution {
+        public ListNode getKthFromEnd(ListNode head, int k) {
+            ListNode result = head;
+            int count = 0;
+            while (head != null) {
+                count++;
+                if (count > k) {
+                    result = result.next;
+                }
+                head = head.next;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Offers022().new Solution().getKthFromEnd(null, 1));
+    }
+}
