6c6870b7c2bf422af5ca34840f9730041e3daf08
kai.dai <kai.dai@qunar.com>
2020-06-22 23:15:08 +0800

    剑指offer

---
+++ src/main/java/com/practice/leetcode/offers/Offers027.java
@@ -0,0 +1,58 @@
+package com.practice.leetcode.offers;
+
+import com.practice.leetcode.assist.TreeNode;
+
+/**
+ * 剑指offer
+ *
+ * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+ *
+ * 例如输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 镜像输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：root = [4,2,7,1,3,6,9]
+ * 输出：[4,7,2,9,6,3,1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author : kai.dai
+ * @date : 2020-05-27 13:34
+ */
+public class Offers027 {
+    class Solution {
+        public TreeNode mirrorTree(TreeNode root) {
+            if (null != root) {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+                mirrorTree(root.left);
+                mirrorTree(root.right);
+            }
+            return root;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Offers027 offers = new Offers027();
+        System.out.println(offers.new Solution().mirrorTree(null));
+    }
+}
